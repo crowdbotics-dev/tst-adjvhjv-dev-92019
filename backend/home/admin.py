001297ff61442d5910523f1311f8d016bc418dd1
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-16 06:54:23 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tempu
+admin.site.register(Tempu)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Tempu(models.Model):
+    'Generated Model'
+    exm = models.BigIntegerField()
